e116eecaa0e7cd287da4b23c950b149224266da2
iosdev747 <iosdev747.icloud@gmail.com>
2021-05-03 19:23:48 +0530

    logs removed, code cleanup

--- ejsapp/cowin-bot/public/post.js
+++ ejsapp/cowin-bot/public/post.js
@@ -6,7 +6,7 @@ const BASE_URL = 'https://cdn-api.co-vin.in/api/v2/admin/location/districts';
 const getDistricts = async (id) => {
     try {
         const response = await axios.get(`${BASE_URL}/${id}`);
-        console.log(response.data)
+        // console.log(response.data)
         // const todoItems = response.data;
         // console.log(`GET: Here's the list of todos`, todoItems);
 
@@ -22,13 +22,13 @@ select.addEventListener('change', async function (event) {
     let e = document.getElementById("states");
     // console.log(e.value);
     let dist = await getDistricts(e.value);
-    console.log(dist.districts);
+    // console.log(dist.districts);
     let x = document.getElementById("dist");
     for (var ii = x.length-1; ii >= 0 ; ii--) {
         x.remove(ii);
     }
     let i;
-    console.log(dist.districts[0]);
+    // console.log(dist.districts[0]);
     // console.log(dist.districts[0].district_name);
     for (i = 0; i < dist.districts.length; i++) {
         var option = document.createElement("option");
--- ejsapp/cowin-bot/routes/index.js
+++ ejsapp/cowin-bot/routes/index.js
@@ -2,34 +2,46 @@ var express = require('express');
 var router = express.Router();
 const axios = require('axios');
 
-// import csc from 'country-state-city';
-// const csc = require('country-state-city');
-
 var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    console.log('hi');
-    // console.log(csc.getStatesOfCountry('IN'));
     states = JSON.parse(fs.readFileSync('public/states.json'));
-    console.log(states.states);
-    res.render('index', {state: states.states, status:false});
+    // console.log(states.states);
+    res.render('index', {state: states.states, status:false, err1:false, err2:false, err3:false});
 });
 
 router.post("/remind", async function (request, response) {
     const email = request.body.email;
     const state = request.body.states;
     const dist = request.body.dist;
-    console.log(email + state + dist);
+    // console.log(email);
+    // console.log(state);
+    // console.log(dist);
+    let err1 = false;
+    let err2 = false;
+    let err3 = false;
+    if(email === "") {
+        err1 = true;
+    }
+    if(state === undefined) {
+        err2 = true;
+    }
+    if(dist === undefined) {
+        err3 = true;
+    }
+    if(err1 || err2 || err3) {
+        response.render('index', {state: states.states, status:false, err1: err1, err2: err2, err3: err3});
+    }
     let payload = { email: email, cityid: dist };
     let res = await axios.post('http://20.197.27.108:7000/api/adduser', payload);
     let data = res.data;
     if(res.status === 200) {
-        response.render('index', {state: states.states, status:true});
+        response.render('index', {state: states.states, status:true, err1: err1, err2: err2, err3: err3});
     } else {
-        response.render('index', {state: states.states, status:false});
+        response.render('index', {state: states.states, status:false, err1: err1, err2: err2, err3: err3});
     }
-    console.log(data);
+    // console.log(data);
 });
 
 module.exports = router;
